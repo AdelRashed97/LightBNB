a375f4ae0bca80f9e97e7b16dec203c6c1afb6ce
Adel Rashed <adel_rashed@outlook.com>
2020-06-25 21:58:54 +0000

    added web app

--- 1_queries/properties_by_city.sql
+++ 1_queries/properties_by_city.sql
@@ -1,7 +1,7 @@
-SELECT properties.*,AVG(rating) as average_rating
+SELECT properties.title,AVG(rating) as average_rating
 FROM properties
 JOIN property_reviews ON property_reviews.property_id = properties.id
-WHERE city = 'Carcross'
+WHERE title = 'Carcross'
 GROUP BY properties.id
 HAVING AVG(rating) >= 4
 ORDER BY cost_per_night
