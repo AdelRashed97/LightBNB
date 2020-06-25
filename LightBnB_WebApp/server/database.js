eb60ebc24879ef1884b6f9e842aec7ae95b5a78f
Adel Rashed <adel_rashed@outlook.com>
2020-06-25 18:57:46 -0400

    getAllproperties by quering the database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -74,11 +74,11 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  const query = `SELECT * FROM properties
+                  LIMIT $1;`;
+  return pool.query(query,[limit])
+    .then(res => res.rows)
+    .catch(err => console.log(err.stack));
 }
 exports.getAllProperties = getAllProperties;
 
